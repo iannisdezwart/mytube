78a82a71ef3d626451b8a75e5980d1094f6c6f9a
sachruhe <sacha.ruhe@gmail.com>
2018-04-25 20:00:34 +0200

    Added main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listening for app
+app.on('ready', function(){
+    // New window
+    mainWindow = new BrowserWindow();
+    // Put html in window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+});
+
+// Close app when exited
+app.on('window-all-closed', () => {
+    app.quit()
+})
